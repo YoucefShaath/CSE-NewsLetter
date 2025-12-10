4ee7b10318ede3346c3912d73f8f0f9fe3dc0e0a
Taha <tbendjedia@gmail.com>
2025-12-10 23:20:59 +0100

    .changed font ..

--- src/Components/ManageSubscriptions/managesub.jsx
+++ src/Components/ManageSubscriptions/managesub.jsx
@@ -44,7 +44,7 @@ export default function SubscriptionPage() {
   };
 
   return (
-    <div className="relative flex min-h-screen w-full flex-col bg-cover bg-[url('/bg-persInfsm.png')]  md:bg-[url('/bg-persInf.png')] bg-dark-blue tracking-[2px] " style={{ fontFamily: 'Times New Roman' }} >
+    <div className="relative flex min-h-screen w-full flex-col bg-cover bg-[url('/bg-persInfsm.png')]  md:bg-[url('/bg-persInf.png')] bg-dark-blue  "  >
       <div className="flex h-full grow flex-col">
         {/* Header */}
         <nav className="flex items-center justify-between whitespace-nowrap border-b-2 border-solid border-gray-900 px-10 py-3 bg-dark-blue/10 backdrop-blur-lg  sticky top-0 z-10 backdrop-blur-sm">
@@ -52,18 +52,16 @@ export default function SubscriptionPage() {
 
           <div className="flex items-center gap-4">
             <Image src="/darklogo.svg" alt="CSE Logo" width={35} height={35} />
-            <h1 className="text-xl font-black tracking-tight text-white pl-[600px]">CSE NEWSLETTER</h1>
+            <h1 className="text-xl font-black tracking-tight text-white md:pl-[600px]">CSE NEWSLETTER</h1>
           </div>
           <div className="flex flex-1 justify-end gap-8 items-center">
-            <div className="flex items-center gap-8">
+            <div className="md:flex items-center gap-8  hidden">
               <a className="text-white dark:text-stone-400 hover:text-light-blue dark:hover:text-stone-200 text-sm font-medium" href="#">Home</a>
               <a className="text-white dark:text-stone-400 hover:text-light-blue dark:hover:text-stone-200 text-sm font-medium" href="#">My Feed</a>
               <a className="text-white dark:text-stone-200 text-sm font-bold border-b-2 border-gray-900 dark:border-stone-200 pb-1" href="#">Subscriptions</a>
             </div>
             <div className="flex gap-2 items-center">
-              <button className="flex cursor-pointer items-center justify-center overflow-hidden rounded-full h-9 w-9 border border-stone-300 dark:border-stone-700 text-gray-600 dark:text-stone-400 hover:bg-black/5 dark:hover:bg-white/5">
-                <span className="text-xl">🔍</span>
-              </button>
+              
               <button className="flex cursor-pointer items-center justify-center overflow-hidden rounded-full h-9 w-9 border border-stone-300 dark:border-stone-700 text-gray-600 dark:text-stone-400 hover:bg-black/5 dark:hover:bg-white/5">
                 <span className="text-xl">🔔</span>
               </button>
