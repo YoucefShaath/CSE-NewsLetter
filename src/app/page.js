72158cb139f5efeab3a3436e409f2698899b5c0b
abdallah-gd <abdallahgaad32@gmail.com>
2025-12-10 01:50:35 +0100

    the departements page, give me your feedback(what I should make better)

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,12 @@
-import Navbar_Dark from "../Components/NavBar/darknavbar.jsx";
-import Navbar_Light from "../Components/NavBar/lightnavbar.jsx";
-import LogIn from "@/Components/LogIn/login.jsx";
-import SignUp from "@/Components/SignUp/signup.jsx";
-import HomePage from "@/Components/Home/home.jsx";
+import DepartementsPage  from  "../Components/departements/departements.jsx";
+
+
 
 export default function Home() {
   return (
     <div>
-      {/* <Navbar_Dark />
-      <HomePage /> */}
-      {/* <LogIn/> */}
-      <SignUp />
+      <DepartementsPage/>
+      {/* <Post/> */}
     </div>
   );
 }
